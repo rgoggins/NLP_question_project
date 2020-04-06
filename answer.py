66aa08c3fd3b0996927230b054ab508eede50ae0
rgoggins <ryanngoggins@gmail.com>
2020-04-06 15:39:42 -0700

    Created initial files

---
+++ answer.py
@@ -0,0 +1,12 @@
+'''
+@file answer.py
+@brief A python script for generating answers to a series of questions
+
+11-411: Natural Language Processing
+
+@author Ryan Goggins <goggins@cmu.edu>
+@author Casey Al-Haddad <chaddad@andrew.cmu.edu>
+@author Justin Lee <justinl2@andrew.cmu.edu>
+@author Oliver Pennington <oop@cmu.edu>
+
+'''
---
+++ ask.py
@@ -0,0 +1,12 @@
+'''
+@file ask.py
+@brief A python script for generating questions about a given document
+
+11-411: Natural Language Processing
+
+@author Ryan Goggins <goggins@cmu.edu>
+@author Casey Al-Haddad <chaddad@andrew.cmu.edu>
+@author Justin Lee <justinl2@andrew.cmu.edu>
+@author Oliver Pennington <oop@cmu.edu>
+
+'''
