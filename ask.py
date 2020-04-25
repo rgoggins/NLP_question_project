edb5d097f3a3e1f63967ca2b395ffca75903eed4
rgoggins <ryanngoggins@gmail.com>
2020-04-25 15:44:55 -0700

    Really solid

--- ask.py
+++ ask.py
@@ -11,6 +11,7 @@
 @author Oliver Pennington <oop@cmu.edu>
 
 '''
+import os
 import re
 import math
 import spacy
@@ -19,6 +20,7 @@ import random
 from tokenizer import *
 import nltk
 from textblob import TextBlob
+nlp = spacy.load('en_core_web_md')
 
 
 # Passed in documents and a number 'n' of questions we are required to generate
@@ -30,8 +32,6 @@ from textblob import TextBlob
 
 tokenized_data = None # call whatever tokenizer.py function gets the data
 num_questions = 10 # this is also passed in
-
-sent = "Although no announcement was made by his team, Ryan Dempsey appeared in the Seattle squad to face Sporting in the season opener on March 10, 2014."
 
 # sentiment_q = False
 
@@ -57,6 +57,8 @@ def generate_when_question(root_sentence):
     newsent = []
     # print("Sentence: " + str(sentencestr))
 
+    altered = False
+
     for i, word in enumerate(sentencestr):
         # print("Iteration " + str(i) + ": " + word)
         if (len(word) == 4 and word.isdigit() and (word[:2] in ["18", "19", "20"])):
@@ -71,21 +73,30 @@ def generate_when_question(root_sentence):
             if (i > 0) and (sentencestr[i-1].lower() == "the"):
                 newsent = newsent[:-1]
                 newsent.append("which year of the")
+                altered = True
                 continue
             if (i > 0) and (sentencestr[i-1].lower() in ["in", "on", "between"]):
                 newsent.append("what year")
+                altered = True
+
                 continue
             else:
+                altered = True
                 newsent.append("in which year")
                 continue
         elif (word.isdigit() and (i < len(sentencestr) - 1) and sentencestr[i+1] in ["BC", "AD", "BCE"]):
             # This is a date
+            altered = True
             newsent.append("in about which year")
 
         elif ((word[0].isdigit()) and (not word[-1].isdigit()) and (i < len(sentencestr) - 2) and (sentencestr[i+1] == "century") and (sentencestr[i+2][:2] in ["BC", "AD", "CE"])):
+            altered = True
             newsent.append("which")
         else:
             newsent.append(word)
+
+    if (not altered):
+        return None
 
     return " ".join(newsent)[:-1] + "?"
 
@@ -150,6 +161,35 @@ def generate_who_question(root_sentence, man, woman, entity):
 
     return altered
 
+def gen_when(f):
+    le = set([(x.text.strip()) for x in list(nlp(f).ents) if str(x.label_) in ["DATE","TIME"]])
+    qcands = []
+    for sent in TextBlob(f).sentences:
+        ssent = str(sent).strip()
+        keywords = ["on", "in", "during"]
+        is_cand = any([ssent.lower().find(keyword) == 0 for keyword in keywords])
+        if is_cand:
+            wo_first = ssent[ssent.find(" "):].strip()
+            for t in le:
+                woi = wo_first.find(t)
+                ssi = ssent.find(t)
+                if woi == 0:
+                    sent_stub = wo_first[len(t) + 1:].strip()
+                    first_word = sent_stub.split(" ")[0].lower()
+                    if not first_word in ["he", "she", "it", "they"]: #avoid ambiguity
+                        if not first_word.replace(",", "").isnumeric(): #avoid misparse
+                            s_formed = ("When is it that " + sent_stub + "?")
+                            qcands.append(s_formed)
+                            break
+                if ssi == 0:
+                    sent_stub = ssent[len(t) + 1:].strip()
+                    first_word = sent_stub.split(" ")[0].lower()
+                    if not first_word in ["he", "she", "it", "they"]: #avoid ambiguity
+                        if not first_word.replace(",", "").isnumeric():
+                            s_formed = ("When is it that " + sent_stub + "?")
+                            qcands.append(s_formed)
+                            break
+    return qcands
 
 def generate_binary_question(root_sentence):
     phrase = "Is it correct that "
@@ -188,7 +228,21 @@ def meets_when_crit(sentence, iter):
     return (len(sentence.words) > 15 - iter) and ('\n' not in str(sentence)) and (sen.tags[0][1] not in invalid_starting_labels) and ('"' not in str(sentence)) and ("for example" not in str(sentence).lower())
 
 
+def subject_not_preposition(sentence):
+    st = str(sentence)
+    doc = nlp(st)
+    subjects = [str(x) for x in doc if (x.dep_ == "nsubj")]
+    if (len(subjects) == 0):
+        return False
+
+    sub = subjects[0]
+    newtk = TextBlob(sub)
+    if (newtk.tags[0][1] == "PRP"):
+        return False
+    return True
+
 if __name__ == "__main__":
+
 
     questions = []
 
@@ -222,13 +276,15 @@ if __name__ == "__main__":
     else:
         woman = True
 
+    when_questions = gen_when(str(tk.blob))
+
     index = 0
     turn = 0
 
     while (len(questions) < num_questions):
         iter = (index // len(tk.blob.sentences))
         sen = tk.blob.sentences[index % len(tk.blob.sentences)]
-        if (sen not in sentence_roots):
+        if (sen not in sentence_roots) and (subject_not_preposition(sen)):
             if (turn % 3 == 0):
                 if (meets_binary_crit(sen, iter)):
                     output = generate_binary_question(sen)
@@ -246,7 +302,11 @@ if __name__ == "__main__":
                         sentence_roots.append(sen)
 
             elif (turn % 3 == 2):
-                if (meets_when_crit(sen, iter)):
+                if (random.randint(1,4) == 1):
+                    if (when_questions != None):
+                        if (len(when_questions) != 0):
+                            questions.append(when_questions.pop(0))
+                elif (meets_when_crit(sen, iter)):
                     output = generate_when_question(sen)
                     if (output != None) and (output not in questions):
                         turn += 1
